c187c5f6e7717056989853602f8b14ffa3549024
˹ʏᴜᴍɪ ꭙ˼ <146187449+YumiXBot@users.noreply.github.com>
2023-10-23 19:04:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
+    "GIT_TOKEN", "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"
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AloneXBots")
