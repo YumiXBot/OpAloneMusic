2b106f320141fd015b308b8674c64bb10f9e94a8
TeamDilwar <132042974+TeamDilwar@users.noreply.github.com>
2023-10-28 14:02:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = int(getenv("API_ID", "14281524"))
 API_HASH = getenv("API_HASH", "1a2e3f00651f595c53c36140abc5d0c9")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "6068942263:AAHrKBfNPdPBNqchYcXBB6jA_8KSwQUJn04)
+BOT_TOKEN = getenv("BOT_TOKEN", "6068942263:AAHrKBfNPdPBNqchYcXBB6jA_8KSwQUJn04")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://hny:zara@cluster0.lfe5o.mongodb.net/?retryWrites=true&w=majority")
