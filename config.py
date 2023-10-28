6b351e3cb8899836b596c641e9b456a5f7eebcd4
TeamDilwar <132042974+TeamDilwar@users.noreply.github.com>
2023-10-28 13:50:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AloneXBot/OneXBot",
+    "https://github.com/TeamDilwar/OneXBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
