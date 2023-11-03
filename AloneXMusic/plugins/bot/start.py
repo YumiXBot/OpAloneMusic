78ca2ad9873236d27dc038ec893d60d892da4a6e
˹ʏᴜᴍɪ ꭙ˼ <146187449+YumiXBot@users.noreply.github.com>
2023-11-04 00:54:53 +0530

    Update start.py

--- AloneXMusic/plugins/bot/start.py
+++ AloneXMusic/plugins/bot/start.py
@@ -86,7 +86,7 @@ async def start_pm(client, message: Message, _):
         out = private_panel(_)
         await message.reply_photo(
             photo=config.START_IMG_URL,
-            caption=_["start_2"].format(message.from_user.mention, app.mention),
+            caption=_["start_2"].format(app.mention, message.from_user.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
         if await is_on_off(2):
